63ab3b9b251c81c18c89b80b4c3208b40b66bf61
ereinha3 <ethanreinhart@gmail.com>
2025-04-11 19:05:35 -0700

    small formatting fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="w-full flex flex-col justify-center items-center text-center -mt-[10vh] mb-[5vh]" id="contact">
-            <div className="text-center text-[2.5rem] my-[10vh] mx-auto">Contact</div>
-            <div className="w-full flex flex-col items-center py-[8vh]">
+            <div className="text-center text-[2.5rem] mb-[1vh] md:mb-[1vh] mx-auto">Contact</div>
+            <div className="w-full flex flex-col items-center py-[4vh] md:py-[8vh]">
                 <div className="text-[2rem]">Want to know more? Let's get in touch.</div>
-                <div className="w-1/2 flex justify-around items-center text-[2rem] my-[8vh] mx-auto">
+                <div className="w-full md:w-1/2 flex flex-col md:flex-row justify-around items-center text-[2rem] my-[4vh] md:my-[8vh] gap-8 md:gap-0">
                     <a 
                         href="https://www.linkedin.com/in/ethan-reinhart-3bb28b285/" 
                         className="hover:underline hover:underline-offset-[1em] hover:text-[#555]"
